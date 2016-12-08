fe9bd65daa9a8b4fd3ba2ba1435c7f7167953f49
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2016-12-08 12:01:28 -0800

    add user model, controller, table

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    @user = current_user
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(session_params[:email], session_params[:password])
+    if user
+      log_in(user)
+      redirect_to user_url(user)
+    else
+      flash.now[:errors] = user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+  def session_params
+    params.require(:user).permit(:email, :password)
+  end
+end
